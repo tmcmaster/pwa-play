5b3686604609ceb86ff6ea4bcf4bb37b80f1a7d8
Tim McMaster <tim@mcmaster.id.au>
2019-05-09 13:12:15 +1000

    added filter support

--- src/views/wired-todo-view.ts
+++ src/views/wired-todo-view.ts
@@ -6,11 +6,18 @@ import {State} from "../models/state";
 //import {getVisibleTodosSelector} from "../redux/reducer";
 import { getVisibleTodosSelector } from '../redux/reducer';
 import { connect } from 'pwa-helpers';
-//import { store } from '../redux/store';
-//import { VisibilityFilter } from '../models/visibility-filter';
+import { VisibilityFilter } from '../models/visibility-filter';
 
-import 'wired-elements';
-import {UpdateTodoValueAction} from "../redux/actions";
+import 'wired-elements/wired-elements';
+//import { fireWindowEvent } from 'wired-elements/lib/wired-lib.js';
+
+import {
+    UpdateTodoValueAction,
+    UpdateFilterAction,
+    AddTodoAction,
+    UpdateTodoAction,
+    ClearCompletedAction
+} from "../redux/actions";
 
 @customElement('wired-todo-view')
 export class TodoView extends connect(store)(BaseView) {
@@ -18,8 +25,8 @@ export class TodoView extends connect(store)(BaseView) {
     private todos: Todo[];
     @property()
     private filter: string;
-    // @property()
-    // private task: string;
+    @property()
+    private task: string;
 
     // noinspection JSUnusedGlobalSymbols
     stateChanged(state: State) {
@@ -32,33 +39,70 @@ export class TodoView extends connect(store)(BaseView) {
         // noinspection CssInvalidPropertyValue,CssInvalidHtmlTagReference
         return html`
             <style>
+                wired-todo-view {
+                    display: inline-block;
+                    height: 100%;
+                    width: 100%;
+                    box-sizing: border-box;
+                }
                 wired-card {
-                    width: 800px;
+                    display: flex;
+                    flex-direction: column;
+                    width: 100%;
                     padding: 20px 10px 30px 10px;
+                    height: 100%;
+                    box-sizing: border-box;
                 }
                 
                 div.add {
-                    padding: 0 20px 0 10px;
+                    flex:min-content;
+                    padding: 20px 40px 0 30px;
                     display: flex;
                     flex-direction: row;
+                    box-sizing: border-box;
+                    width: 100%;
                 }
                 
                 div.add > wired-input {
                     flex: auto;
                 }
-                div.add > wired-input {
-                    width: 100px;
+                div.add > wired-button {
+                    margin-left: 20px;
+                }
+                div.scroll {
+                    display: inline-block;
+                    flex: auto;
+                    width: 100%;
+                    box-sizing: border-box;
                 }
                 div.list {
+                    display: inline-block;
                     padding: 20px 20px 0 50px;
+                    width: 100%;
+                    box-sizing: border-box;
                 }
                 
-                wired-input.list {
-                    width: 600px;
+                
+                div.row {
+                    box-sizing: border-box;
+                    display: flex;
+                    flex-direction: row;
+                    width: 100%;
+                    padding-right: 40px;
+                }
+                
+                div.row > wired-input {
+                    flex: auto;
+                }
+                
+                div.row > wired-checkbox {
+                    width:50px;
                 }
                 
                 div.filter {
-                    padding: 20px 40px 0 20px;
+                    flex:min-content;
+                    //height: 100px;
+                    padding: 20px 40px 30px 30px;
                 }
                 
                 div.filter > wired-radio-group {
@@ -67,38 +111,103 @@ export class TodoView extends connect(store)(BaseView) {
                 div.filter > wired-button {
                     float: right;
                 }
+                
+                div.layout {
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: stretch;
+                }
             </style>
-            <wired-card>
+            <wired-card id="card" mode="stretch">
+            <div class="layout">
                 <div class="add">
-                    <wired-input value="${this.filter}"></wired-input>
-                    <wired-button>Add Todo</wired-button>
+                    <wired-input 
+                        placeholder="Task"
+                        value="${this.task || ''}"
+                        @change="${this.updateTask}"
+                    ></wired-input>
+                    <wired-button
+                        @click="${this.addTodo}"
+                    >Add Todo</wired-button>
                 </div>
-                <div class="list">                
-                    ${
-                        this.todos.map(todo => html`
-                            <wired-checkbox/>
-                            <wired-input class="list"
-                                @change="${(e: { target: HTMLInputElement }) => this.updateTodoValue(todo, e.target.value)}" 
-                                value="${todo.task}"
-                            ></wired-input>
-                        `)
-                    }
+                <div class="scroll">      
+                    <div class="list">                
+                        ${
+                            this.todos.map(todo => html`
+                                <div class="row">
+                                    <wired-checkbox
+                                        ?checked="${todo.complete}"
+                                    ></wired-checkbox>
+                                    <wired-input class="list"
+                                        @change="${(e: { target: HTMLInputElement }) => this.updateTodoValue(todo, e.target.value)}" 
+                                        value="${todo.task}"
+                                    ></wired-input>
+                                </div>
+                            `)
+                        }
+                    </div>
                 </div>
                 <div class="filter">
-                    <wired-radio-group id="rg" selected="${this.filter}">
-                        <wired-radio name="All">All</wired-radio>
-                        <wired-radio name="Active">Two</wired-radio>
-                        <wired-radio name="Complete">Complete</wired-radio>
+                    <wired-radio-group
+                        selected="${this.filter}"
+                        @selected="${this.filterChanged}"
+                    >
+                        ${
+                            Object.values(VisibilityFilter).map(
+                                filter => html `
+                                    <wired-radio name="${filter}">${filter}</wired-radio>
+                                `
+                            )
+                        }
                     </wired-radio-group>
-                    <wired-button>Clear Complete</wired-button>
+                    <wired-button
+                        
+                    >Clear Complete</wired-button>
+                </div>
                 </div>
             </wired-card>
+           
         `;
     }
 
+    filterChanged(e: { detail: { selected: VisibilityFilter } }) {
+        console.log('Filter Changed: ', e.detail.selected);
+        store.dispatch(new UpdateFilterAction(e.detail.selected));
+        //fireWindowEvent('refresh-element');
+        //window.dispatchEvent(new CustomEvent('resize-element'));
+    }
 
     // noinspection JSMethodCanBeStatic
     updateTodoValue(updatedTodo: Todo, value: string) {
         store.dispatch(new UpdateTodoValueAction(updatedTodo, value));
+    }
+
+    addTodo() {
+        if (this.task) {
+            store.dispatch(new AddTodoAction(this.task));
+            this.task = '';
+        }
+    }
+
+    shortcutListener(e: KeyboardEvent) {
+        if (e.key === 'Enter') {
+            this.addTodo();
+        }
+    }
+
+    updateTask(e: { target: HTMLInputElement }) {
+        this.task = e.target.value;
+    }
+
+    // noinspection JSMethodCanBeStatic
+    updateTodoStatus(updatedTodo: Todo, complete: boolean) {
+        store.dispatch(new UpdateTodoAction(updatedTodo, complete));
+    }
+
+    // noinspection JSMethodCanBeStatic
+    clearCompleted() {
+        store.dispatch(new ClearCompletedAction());
     }
 }
